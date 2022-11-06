a0588e26614a36d22adaf350d134076468f58651
Benjamin Shuster <77933963+bentzi-shuster@users.noreply.github.com>
2022-11-06 11:34:52 -0500

    more changes

--- web/src/components/Home/Home.svelte
+++ web/src/components/Home/Home.svelte
@@ -4,13 +4,14 @@
 <h1>Home</h1>
 
 <div class="sources">
+    <p>Tools used:</p>
 <a href="https://supabase.com/">Supabase</a>
 <a href="https://github.com/">Github</a>
 <a href="https://svelte.dev/">Svelte</a>
 <a href="https://vitejs.dev/">Vite</a>
 <a href="https://incarnate.github.io/curl-to-php/">Curl-to-PHP</a>
 <a href="https://www.arduino.cc/">Arduino & Arduino IDE</a>
-<a href="">FiraCode</a>
+<a href="https://github.com/tonsky/FiraCode/releases">FiraCode</a>
 
 </div>
 <style>
@@ -21,5 +22,16 @@ h1{
 }
 p{
     color: white;
+}
+.sources{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    flex-direction: column;
+    
+}
+a{
+    color: white ;
 }
 </style>
